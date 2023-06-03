8c4dc07aec741dfaa0f81543bca95e30f5d980f8
Karl-Markus <karl-markus.vaher@voco.ee>
2023-06-03 11:52:24 +0300

    Elering päringu modifitseerimine

--- nordpool.ts
+++ nordpool.ts
@@ -10,4 +10,27 @@ router.get("/nord-pool-price", async (req: Request, res: Response) => {
     res.json(response.data);
 });
 
+
+router.get("/nord-pool-price/:country", async (req: Request, res: Response) => {
+    const response = await axios.get(
+        "https://dashboard.elering.ee/api/nps/price"
+    );
+    switch(req.params.country) {
+        case "ee":
+            res.json(response.data.data.ee);
+            break;
+        case "lv":
+            res.json(response.data.data.lv);
+            break;
+        case "lt":
+            res.json(response.data.data.lt);
+            break;
+        case "fi":
+            res.json(response.data.data.fi);
+            break;
+        default:
+            res.json([]);
+    }
+});
+
 export default router;
